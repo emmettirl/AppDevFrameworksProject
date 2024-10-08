c5cbe373cac8cb8bf73021b208f9725f49f34bce
Emmett <emmett.fitzharris@mycit.ie>
2024-10-08 14:10:29 +0100

    Initial Commit

---
+++ src/main/java/app/dev/framworks/appdevframeworksproject/AppDevFrameworksProjectApplication.java
@@ -0,0 +1,13 @@
+package app.dev.framworks.appdevframeworksproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDevFrameworksProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppDevFrameworksProjectApplication.class, args);
+    }
+
+}
